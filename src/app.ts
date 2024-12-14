af62e14805b094781204ff8c77ba3b07b0b9edcc
Taline <talinedacosta@gmail.com>
2024-12-14 12:24:12 -0300

    feat: add cors settings

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,18 @@ import postsRoutes from "@/http/controllers/post/routes";
 import userRoutes from "@/http/controllers/user/routes";
 import { globalErrorHandler } from "./utils/global-error-handler";
 import setupSwagger from "swagger";
+import cors from "cors";
 
 export const app = express();
 setupSwagger(app);
 
 app.use(express.json({ limit: "10mb" }));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}))
 
 app.get("/", (_, res) => {
   res.status(200).send("Hello World!");
